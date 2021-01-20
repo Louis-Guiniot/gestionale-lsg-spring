32310c5c1f1b54d3148cc99c4051d482eff9cc0a
LocaTheProgrammer <pietro.locatelli00@gmail.com>
2021-01-20 14:54:57 +0100

    creazione entità "product"

---
+++ jac/src/main/java/it/gestionalejaclsg/jac/entity/Product.java
@@ -0,0 +1,44 @@
+package it.gestionalejaclsg.jac.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+
+import lombok.Data;
+
+@Entity
+@Table(name = "product")
+@Data
+public class Product {
+	
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name = "code")
+	private String codeProduct;
+
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "description")
+	private String description;
+	
+	@Column(name = "price")
+	private String price;
+	
+	
+	//unità di misura migrata da entità unità di misura
+	@Column(name = "measureUnit")
+	private String measureUnit;
+
+
+
+	
+}
---
+++ jac/src/main/java/it/gestionalelsg/jac/configuration/AppConfiguration.java
@@ -0,0 +1,36 @@
+package it.gestionalelsg.jac.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
+public class AppConfiguration extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
+
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+		.csrf().disable()
+		.authorizeRequests()
+		.antMatchers().permitAll()//end point senza controllo di nessun tipo, quindi accessibile ad ogni client
+		.anyRequest().permitAll()
+		.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+	}
+
+	@Bean
+	public AuthenticationManager customAuthenticationManager() throws Exception {
+		return authenticationManager();
+	}
+
+}
