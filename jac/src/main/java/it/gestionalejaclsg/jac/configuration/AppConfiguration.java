e3f79a575bf8f293f4d8d488ee71c3311eabe07a
LocaTheProgrammer <pietro.locatelli00@gmail.com>
2021-01-21 14:11:23 +0100

    cors

--- jac/src/main/java/it/gestionalejaclsg/jac/configuration/AppConfiguration.java
+++ jac/src/main/java/it/gestionalejaclsg/jac/configuration/AppConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 //import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -28,6 +29,12 @@ public class AppConfiguration extends WebSecurityConfigurerAdapter implements We
 		.anyRequest().permitAll()
 		.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
+	}
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/rest/product/**").allowedMethods("GET", "POST");
+		
+		//registry.addMapping("/rest/user/**").allowedMethods("GET", "POST").allowedOrigins("localhost:4200/**");
 	}
 
 	@Bean
