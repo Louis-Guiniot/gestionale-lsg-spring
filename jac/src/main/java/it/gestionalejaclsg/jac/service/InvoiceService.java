4e34b66762fd6a64c30479f93daff3523528720a
LocaTheProgrammer <pietro.locatelli00@gmail.com>
2021-01-25 22:45:53 +0100

    ok

--- jac/src/main/java/it/gestionalejaclsg/jac/service/InvoiceService.java
+++ jac/src/main/java/it/gestionalejaclsg/jac/service/InvoiceService.java
@@ -9,7 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import it.gestionalejaclsg.jac.controller.InvoiceRestController;
+
 import it.gestionalejaclsg.jac.dao.InvoiceRepository;
 import it.gestionalejaclsg.jac.dto.InvoiceDTO;
 import it.gestionalejaclsg.jac.dto.Response;
