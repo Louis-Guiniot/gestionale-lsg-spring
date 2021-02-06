7dc865b395860fd457aef7a3e35f8fec79a30a4e
Pietro Locatelli <pietro.locatelli@stu.fondazionejobsacademy.org>
2021-02-06 11:00:14 +0100

    invoice commentata

--- jac/src/main/java/it/gestionalejaclsg/jac/controller/InvoiceRestController.java
+++ jac/src/main/java/it/gestionalejaclsg/jac/controller/InvoiceRestController.java
@@ -27,7 +27,7 @@ public class InvoiceRestController {
 	@PostMapping("/create")
 	public Response<?> createInvoice(@RequestBody String body) {
 
-		//
+		//{products,date,sale,imponibile,condizionePagamento,docType,idCustomer}
 
 		log.info("\n\n\n\nbody: " + body + "\n\n\n");
 		int conta = 0;
@@ -38,86 +38,87 @@ public class InvoiceRestController {
 				conta++;
 			}
 		}
-		String lista = body.substring(arr[2] + 1, arr[3]);
-		String price = body.substring(arr[6] + 1, arr[7]);
-		String customerId = body.substring(arr[10] + 1, arr[11]);
-		String sconto;
-		if((body.substring(arr[14]+1,arr[15])).equals("")) {
-			sconto="0";
-		}else {
-			sconto=body.substring(arr[14]+1,arr[15]);
-			
-		}
-		
-		
-		price = price.substring(0, price.length() - 1);
-		
-		int conta2 = 0;
-		int[] array = new int[price.length()];
-		for (int i = 0; i < price.length(); i++) {
-			if (price.charAt(i) == ';') {
-				array[conta2] = i;
-				conta2++;
-			}
-		}
-		log.info("CONTA 2 VALE: " + conta2);
-		String[] pricesArrayString = price.split(";", conta2 + 1);
-
-		log.info("inizio ciclo di stampa prezzi:");
-		for (int i = 0; i < pricesArrayString.length; i++) {
-			log.info(pricesArrayString[i]);
-		}
-
-		log.info("ciclo di conversione");
-		double[] pricesArrayDouble = new double[pricesArrayString.length];
-		for (int i = 0; i < pricesArrayString.length; i++) {
-			if(pricesArrayString[i].equals("undefined")) {
-				pricesArrayString[i]="0";
-			}
-			pricesArrayDouble[i] = Integer.parseInt(pricesArrayString[i]);
-		}
-
-		log.info("ciclo di somma");
-		double finalPrice = 0;
-		for (int i = 0; i < pricesArrayDouble.length; i++) {
-			finalPrice += pricesArrayDouble[i];
-		}
-
-		// generazione codice casuale
-		int leftLimit = 48; // numeral '0'
-		int rightLimit = 122; // letter 'z'
-		int targetStringLength = 10;
-		Random random = new Random();
-
-		String generatedString = random.ints(leftLimit, rightLimit + 1)
-				.filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97)).limit(targetStringLength)
-				.collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append).toString();
-		// fine coidce casuale
-
-		log.info("final price: " + finalPrice);
-		log.info("creazione invoice");
-
-		String str = "";
-		Invoice invoice = new Invoice();
-
-		Calendar calndr = Calendar.getInstance();
-
-		invoice.setDateTime(calndr.getTime().toString());
-		invoice.setTotalPrice(str + finalPrice);
-		invoice.setFields(lista);
-		invoice.setIdCustomer(customerId);
-		invoice.setCodeInvoice(generatedString);
-		invoice.setSconto(sconto);
-		invoice.setIva("22");
-		invoice.setCustomer_id(Integer.parseInt(customerId));
-		
-		double ivaPriceDouble=finalPrice-(finalPrice*0.22);
-		invoice.setIvaPrice(str+ivaPriceDouble);
-		
-		//double ttp=ivaPriceDouble-(ivaPriceDouble*(Double.parseDouble(sconto)/100));
-		invoice.setTotalToPay(str+finalPrice);
-		
-		return this.invoiceService.createInvoice(invoice);
+//		String lista = body.substring(arr[2] + 1, arr[3]);
+//		String price = body.substring(arr[6] + 1, arr[7]);
+//		String customerId = body.substring(arr[10] + 1, arr[11]);
+//		String sconto;
+//		if((body.substring(arr[14]+1,arr[15])).equals("")) {
+//			sconto="0";
+//		}else {
+//			sconto=body.substring(arr[14]+1,arr[15]);
+//			
+//		}
+//		
+//		
+//		price = price.substring(0, price.length() - 1);
+//		
+//		int conta2 = 0;
+//		int[] array = new int[price.length()];
+//		for (int i = 0; i < price.length(); i++) {
+//			if (price.charAt(i) == ';') {
+//				array[conta2] = i;
+//				conta2++;
+//			}
+//		}
+//		log.info("CONTA 2 VALE: " + conta2);
+//		String[] pricesArrayString = price.split(";", conta2 + 1);
+//
+//		log.info("inizio ciclo di stampa prezzi:");
+//		for (int i = 0; i < pricesArrayString.length; i++) {
+//			log.info(pricesArrayString[i]);
+//		}
+//
+//		log.info("ciclo di conversione");
+//		double[] pricesArrayDouble = new double[pricesArrayString.length];
+//		for (int i = 0; i < pricesArrayString.length; i++) {
+//			if(pricesArrayString[i].equals("undefined")) {
+//				pricesArrayString[i]="0";
+//			}
+//			pricesArrayDouble[i] = Integer.parseInt(pricesArrayString[i]);
+//		}
+//
+//		log.info("ciclo di somma");
+//		double finalPrice = 0;
+//		for (int i = 0; i < pricesArrayDouble.length; i++) {
+//			finalPrice += pricesArrayDouble[i];
+//		}
+//
+//		// generazione codice casuale
+//		int leftLimit = 48; // numeral '0'
+//		int rightLimit = 122; // letter 'z'
+//		int targetStringLength = 10;
+//		Random random = new Random();
+//
+//		String generatedString = random.ints(leftLimit, rightLimit + 1)
+//				.filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97)).limit(targetStringLength)
+//				.collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append).toString();
+//		// fine coidce casuale
+//
+//		log.info("final price: " + finalPrice);
+//		log.info("creazione invoice");
+//
+//		String str = "";
+//		Invoice invoice = new Invoice();
+//
+//		Calendar calndr = Calendar.getInstance();
+//
+//		invoice.setDateTime(calndr.getTime().toString());
+//		invoice.setTotalPrice(str + finalPrice);
+//		invoice.setFields(lista);
+//		invoice.setIdCustomer(customerId);
+//		invoice.setCodeInvoice(generatedString);
+//		invoice.setSconto(sconto);
+//		invoice.setIva("22");
+//		invoice.setCustomer_id(Integer.parseInt(customerId));
+//		
+//		double ivaPriceDouble=finalPrice-(finalPrice*0.22);
+//		invoice.setIvaPrice(str+ivaPriceDouble);
+//		
+//		//double ttp=ivaPriceDouble-(ivaPriceDouble*(Double.parseDouble(sconto)/100));
+//		invoice.setTotalToPay(str+finalPrice);
+//		
+		//return this.invoiceService.createInvoice(invoice);
+		return null;
 	}
 	
 	
