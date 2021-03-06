e8048ae2487bbcbcbe74361aeea3393340f312b9
Pietro Locatelli <pietro.locatelli@stu.fondazionejobsacademy.org>
2021-03-06 15:37:51 +0100

    pc

--- jac/src/main/java/it/gestionalejaclsg/jac/controller/PayConditionRestController.java
+++ jac/src/main/java/it/gestionalejaclsg/jac/controller/PayConditionRestController.java
@@ -8,21 +8,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import it.gestionalejaclsg.jac.dto.Response;
-import it.gestionalejaclsg.jac.service.IvaService;
+
 import it.gestionalejaclsg.jac.service.PayConditionService;
 
 @RestController
 @RequestMapping("/rest/paycondition")
 public class PayConditionRestController {
 
-		private static Logger log = LoggerFactory.getLogger(IvaRestController.class);
+		private static Logger log = LoggerFactory.getLogger(PayConditionRestController.class);
 		
 		@Autowired
 		private PayConditionService payConditionService;
 		
 		
 		@GetMapping(path = "/findAll")
-		public Response<?> findAllIvas() {
+		public Response<?> findAllPayCond() {
 
 			log.info("\n\n\nfind all payconditions");
 			
