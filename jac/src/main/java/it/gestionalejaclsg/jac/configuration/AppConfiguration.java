440ae15060802cddfbf54c9e0d3dc548da4e900c
Louis-Guiniot <louis99679@gmail.com>
2021-03-06 15:32:42 +0100

    cors paycondition

--- jac/src/main/java/it/gestionalejaclsg/jac/configuration/AppConfiguration.java
+++ jac/src/main/java/it/gestionalejaclsg/jac/configuration/AppConfiguration.java
@@ -37,6 +37,7 @@ public class AppConfiguration extends WebSecurityConfigurerAdapter implements We
 		registry.addMapping("/rest/customer/**").allowedMethods("GET", "POST");
 		registry.addMapping("/rest/measure/**").allowedMethods("GET", "POST");
 		registry.addMapping("/rest/iva/**").allowedMethods("GET", "POST");
+		registry.addMapping("/rest/paycondition/**").allowedMethods("GET", "POST");
 		//registry.addMapping("/rest/user/**").allowedMethods("GET", "POST").allowedOrigins("localhost:4200/**");
 	}
 
