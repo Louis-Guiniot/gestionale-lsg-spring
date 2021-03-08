3d88282ea525a14b7ddf46c7178cedb646f7530e
LocaTheProgrammer <pietro.locatelli00@gmail.com>
2021-03-08 21:58:50 +0100

    cors doctype

--- jac/src/main/java/it/gestionalejaclsg/jac/configuration/AppConfiguration.java
+++ jac/src/main/java/it/gestionalejaclsg/jac/configuration/AppConfiguration.java
@@ -39,6 +39,7 @@ public class AppConfiguration extends WebSecurityConfigurerAdapter implements We
 		registry.addMapping("/rest/iva/**").allowedMethods("GET", "POST");
 		registry.addMapping("/rest/paycondition/**").allowedMethods("GET", "POST");
 		registry.addMapping("/rest/producthasinvoice/**").allowedMethods("GET", "POST");
+		registry.addMapping("/rest/doctype/**").allowedMethods("GET", "POST");
 		//registry.addMapping("/rest/user/**").allowedMethods("GET", "POST").allowedOrigins("localhost:4200/**");
 	}
 
